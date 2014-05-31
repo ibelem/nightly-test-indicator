84a6be6f3e680a044cdaeb91c2b7780e0379ceca
Belem Zhang <belem.zhang@intel.com>
2014-05-31 16:41:11 +0800

    v0.02

--- init.py
+++ init.py
@@ -4,7 +4,10 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpclient
 import tornado.options
+import os
 import uimodules
+
+from tornado.template import Template
 
 from tornado.options import define, options
 define('port', default=8888, help='run the give port', type=int)
@@ -12,12 +15,14 @@ define('port', default=8888, help='run the give port', type=int)
 v_proxy_host = 'proxy.jf.intel.com'
 v_proxy_port = 911
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application([
-        (r"/", uimodules.HomeHandler),
-        (r"/report/([0-9]+)", uimodules.ReportHandler),
-        (r"/ansc", AsycHandler),
+        (r'/', uimodules.HomeHandler),
+        (r'/report', uimodules.ReportHandler),
+        (r'/report/([0-9]+)', uimodules.ReportDetailHandler),
+        (r'/form', uimodules.FormHandler)
+        #template_path = os.path.join(os.path.dirname(__file__), 'templates')
     ])
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
--- uimodules.py
+++ uimodules.py
@@ -5,17 +5,27 @@ class HomeHandler(tornado.web.RequestHandler):
     def get(self):
         #entries = self.db.query("SELECT * FROM entries ORDER BY date DESC")
         #entry = self.db.get("SELECT * FROM entries WHERE id = %s", entry_id)
-        entries = ['i7', 'xeon', '中国汉字']
+        entries = ['x', 'xx', 'xxx']
         if not entries: raise tornado.web.HTTPError(404)
         self.render("view/home.htm", title="Crosswalk Nightly Test", entries=entries)
- 
+
 class ReportHandler(tornado.web.RequestHandler):
+     def get(self):
+         entries = ['x', 'xx', 'xxx']
+         if not entries: raise tornado.web.HTTPError(404)
+         self.render("view/report.htm", title="Crosswalk Nightly Test Report", entries=entries)
+         
+class ReportDetailHandler(tornado.web.RequestHandler):
     def get(self, entry_id):
-        entries = ['i7', 'xeon', '中国汉字']
+        entries = ['x', 'xx', 'xxx']
         entry = entries[int(entry_id)]
         if not entries: raise tornado.web.HTTPError(404)
-        self.render("view/report.htm", title="Crosswalk Nightly Test Report", entries=entries, entry=entry)
+        self.render("view/reportdetail.htm", title="Crosswalk Nightly Test Report", entries=entries, entry=entry)
 
-
+class FormHandler(tornado.web.RequestHandler):
+    def post(self):
+        usrname = self.get_argument('usrname')
+        psd = self.get_argument('psd')
+        self.render("view/form.htm", usr=usrname, pd=psd)
  
  
